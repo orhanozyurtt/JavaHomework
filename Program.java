74d24197e23e5db7831d9f9c052821d585aff87a
orhanozyurtt <77588239+orhanozyurtt@users.noreply.github.com>
2023-01-16 21:04:23 +0300

    Add files via upload

--- Program.java
+++ Program.java
@@ -1,22 +1,34 @@
-
 import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Ücret Tutarını Giriniz");
-        double para=0,kdv1=18,kdv2 = 8,kdvHesap=0,sonuc=0;
-        para = input.nextFloat();
 
-        kdvHesap = para < 1000 ? ((para*kdv2)/100) : ((para*kdv1)/100);
-        System.out.println("ana para : "+para);
-        System.out.println("toplam kdv tutarı : "+kdvHesap);
-        sonuc = para + kdvHesap;
-        System.out.println("kdv li tutar : "+sonuc);
+        float s1,s2;
+        int secim;
+        Scanner sc = new Scanner(System.in);
 
-
-
-
-
+        System.out.println("Sayı 1'i girin");
+        s1 = sc.nextInt();
+        System.out.println("Sayı 2'yi girin");
+        s2 = sc.nextInt();
+        System.out.println("işlem seçin\n 1.toplama\n2.çarpma\n3.bölme\n4.çıkarma");
+        secim = sc.nextInt();
+      switch (secim){
+          case 1:
+              System.out.println("sonuç : "+(s1+s2));break;
+          case 2:
+              System.out.println("sonuç : "+(s1*s2));break;
+          case 3:
+              if (s2 != 0) {
+                  System.out.println("sonuç : " + (s1 / s2));
+              }else {
+                  System.out.println("herhangi bir Sayı sıfıra bölünemez");
+              }
+              break;
+          case 4:
+              System.out.println("sonuç : "+(s1-s2) );;
+          default:
+              System.out.println("Geçersiz işlem");
+      }
 
     }
 }
